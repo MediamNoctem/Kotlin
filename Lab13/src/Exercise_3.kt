63cb8e619a1b855e6ee750ac0196d48b76008594
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-04-11 21:19:12 +0300

    Add exercise 5

--- Lab13/src/Exercise_3.kt
+++ Lab13/src/Exercise_3.kt
@@ -4,4 +4,16 @@ fun hello(name: String){
 fun main(){
     val name = readLine()
     hello("$name")
+    println("What is your favorite language?")
+    println("1. C++")
+    println("2. C#")
+    println("3. Java")
+    println("4. Kotlin")
+    println("5. Prolog")
+    val i = readLine()
+
+    when (i) {
+        "1","2","3" -> println("Okay, you aren't toady.")
+    }
+    if (i == "4" || i == "5") println("You are toady!")
 }
