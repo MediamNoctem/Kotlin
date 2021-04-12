8d3fc3ce838aaa421775345fcde1be0957fa7a0d
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-04-12 08:01:23 +0300

    Update exercise 7.3 and add exercise 7.1

--- Lab13/src/Exercise_6.kt
+++ Lab13/src/Exercise_6.kt
@@ -2,16 +2,26 @@ import kotlin.math.absoluteValue
 
 fun main(){
     val x = 914158
-    val sum = sumDigitsNum(x.absoluteValue)
-    println(sum)
-    val mul = mulDigitNum(x.absoluteValue)
-    println(mul)
+    println(sumDigitsNum(x.absoluteValue))
+    println(mulDigitNum(x.absoluteValue))
+    println(maxDigit(x.absoluteValue))
 }
 fun sumDigitsNum(Number: Int): Int {
     if (Number == 0) return 0
     return (Number % 10 + sumDigitsNum(Number / 10))
 }
-fun mulDigitNum(Number: Int): Int {
+fun maxDigit(Number: Int): Int = maxDigit(Number/10,Number % 10)
+fun maxDigit(Number: Int, CurMax: Int): Int {
+    if (Number == 0) return CurMax
+    return if (Number % 10 > CurMax) maxDigit(Number/10,Number % 10)
+    else maxDigit(Number/10,CurMax)
+}
+fun mulDigitNum(Number: Int): Int =
+    when(Number) {
+        in 0..9 -> Number
+        else -> mulDigitNum1(Number)
+    }
+fun mulDigitNum1(Number: Int): Int {
     if (Number == 0) return 1
-    return ((Number % 10) * mulDigitNum(Number / 10))
+    return ((Number % 10) * mulDigitNum1(Number / 10))
 }
