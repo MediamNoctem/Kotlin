e0f21a148db24e40f27b937259417e199ee2f784
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-04-12 08:12:00 +0300

    Add exercise 7.2

--- Lab13/src/Exercise_6.kt
+++ Lab13/src/Exercise_6.kt
@@ -1,10 +1,12 @@
 import kotlin.math.absoluteValue
 
 fun main(){
-    val x = 914158
-    println(sumDigitsNum(x.absoluteValue))
-    println(mulDigitNum(x.absoluteValue))
-    println(maxDigit(x.absoluteValue))
+    val y = 914158
+    val x = y.absoluteValue
+    println(sumDigitsNum(x))
+    println(mulDigitNum(x))
+    println(maxDigit(x))
+    println(minDigit(x))
 }
 fun sumDigitsNum(Number: Int): Int {
     if (Number == 0) return 0
@@ -15,6 +17,12 @@ fun maxDigit(Number: Int, CurMax: Int): Int {
     if (Number == 0) return CurMax
     return if (Number % 10 > CurMax) maxDigit(Number/10,Number % 10)
     else maxDigit(Number/10,CurMax)
+}
+fun minDigit(Number: Int): Int = minDigit(Number/10,Number % 10)
+fun minDigit(Number: Int, CurMin: Int): Int {
+    if (Number == 0) return CurMin
+    return if (Number % 10 < CurMin) minDigit(Number/10,Number % 10)
+    else minDigit(Number/10,CurMin)
 }
 fun mulDigitNum(Number: Int): Int =
     when(Number) {
