52d4224169224440b59cba75d13870e1798e36a8
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-04-12 07:34:39 +0300

    Add exercise 7.3

--- Lab13/src/Exercise_6.kt
+++ Lab13/src/Exercise_6.kt
@@ -1,11 +1,17 @@
 import kotlin.math.absoluteValue
 
 fun main(){
-    val x = 914058
+    val x = 914158
     val sum = sumDigitsNum(x.absoluteValue)
     println(sum)
+    val mul = mulDigitNum(x.absoluteValue)
+    println(mul)
 }
 fun sumDigitsNum(Number: Int): Int {
     if (Number == 0) return 0
     return (Number % 10 + sumDigitsNum(Number / 10))
+}
+fun mulDigitNum(Number: Int): Int {
+    if (Number == 0) return 1
+    return ((Number % 10) * mulDigitNum(Number / 10))
 }
