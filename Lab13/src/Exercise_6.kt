96dd22c582d4b0d9a25d4749a291675f667ef420
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-04-16 19:48:43 +0300

    Add exercise 8.2

--- Lab13/src/Exercise_6.kt
+++ Lab13/src/Exercise_6.kt
@@ -2,59 +2,71 @@ import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 import kotlin.math.floor
 fun main(){
-    val y = 36
+    val y = 103
     val x = y.absoluteValue
     println("Сумма цифр = " + sumDigitsNum(x))
     println("Произведение цифр = " + mulDigitNum(x))
     println("Максимальная цифра = " + maxDigit(x))
     println("Минимальная цифра = " + minDigit(x))
     println("Сумма непростых делителей = " + sumNonPrimeDiv(x))
+    println("Количество цифр, меньших 3 = " + countDigitsNumLess3(x))
 }
 // 6
-fun sumDigitsNum(Number: Int): Int {
-    if (Number == 0) return 0
-    return (Number % 10 + sumDigitsNum(Number / 10))
+fun sumDigitsNum(number: Int): Int {
+    if (number == 0) return 0
+    return (number % 10 + sumDigitsNum(number / 10))
 }
 // 7
-fun maxDigit(Number: Int): Int = maxDigit(Number/10,Number % 10)
-fun maxDigit(Number: Int, CurMax: Int): Int {
-    if (Number == 0) return CurMax
-    return if (Number % 10 > CurMax) maxDigit(Number/10,Number % 10)
-    else maxDigit(Number/10,CurMax)
+fun maxDigit(number: Int): Int = maxDigit(number/10,number % 10)
+fun maxDigit(number: Int, curMax: Int): Int {
+    if (number == 0) return curMax
+    return if (number % 10 > curMax) maxDigit(number/10,number % 10)
+    else maxDigit(number/10,curMax)
 }
-fun minDigit(Number: Int): Int = minDigit(Number/10,Number % 10)
-fun minDigit(Number: Int, CurMin: Int): Int {
-    if (Number == 0) return CurMin
-    return if (Number % 10 < CurMin) minDigit(Number/10,Number % 10)
-    else minDigit(Number/10,CurMin)
+fun minDigit(number: Int): Int = minDigit(number/10,number % 10)
+fun minDigit(number: Int, curMin: Int): Int {
+    if (number == 0) return curMin
+    return if (number % 10 < curMin) minDigit(number/10,number % 10)
+    else minDigit(number/10,curMin)
 }
-fun mulDigitNum(Number: Int): Int =
-    when(Number) {
-        in 0..9 -> Number
-        else -> mulDigitNum1(Number)
+fun mulDigitNum(number: Int): Int =
+    when(number) {
+        in 0..9 -> number
+        else -> mulDigitNum1(number)
     }
-fun mulDigitNum1(Number: Int): Int {
-    if (Number == 0) return 1
-    return ((Number % 10) * mulDigitNum1(Number / 10))
+fun mulDigitNum1(number: Int): Int {
+    if (number == 0) return 1
+    return ((number % 10) * mulDigitNum1(number / 10))
 }
 // 8_1
 // Проверка числа на простоту.
-fun isPrime(Number: Int): Boolean {
-    return if (Number < 3) true else isPrime(2,floor(sqrt(Number.toFloat())),Number)
+fun isPrime(number: Int): Boolean {
+    return if (number < 3) true else isPrime(2,floor(sqrt(number.toFloat())),number)
 }
-fun isPrime(I: Int, T: Float, Number: Int): Boolean {
-    return if (I <= T) {
-        if (Number % 2 != 0) {
-            if (Number % I != 0) isPrime(I+1,T,Number)
+fun isPrime(i: Int, t: Float, number: Int): Boolean {
+    return if (i <= t) {
+        if (number % 2 != 0) {
+            if (number % i != 0) isPrime(i+1,t,number)
             else false
         } else false
     } else true
 }
 // Ищем сумму непростых делителей числа.
-fun sumNonPrimeDiv(Number: Int): Int = sumNonPrimeDiv(Number,0,Number)
-fun sumNonPrimeDiv(CurDiv: Int, CurSum: Int, Number: Int): Int {
-    if (CurDiv == 0) return CurSum
-    val newSum: Int = if (Number % CurDiv == 0 && !(isPrime(CurDiv))) CurSum + CurDiv
-    else CurSum
-    return sumNonPrimeDiv(CurDiv-1, newSum, Number)
+fun sumNonPrimeDiv(number: Int): Int = sumNonPrimeDiv(number,0,number)
+fun sumNonPrimeDiv(curDiv: Int, curSum: Int, number: Int): Int {
+    if (curDiv == 0) return curSum
+    val newSum: Int = if (number % curDiv == 0 && !(isPrime(curDiv))) curSum + curDiv
+    else curSum
+    return sumNonPrimeDiv(curDiv-1, newSum, number)
+}
+// 8_2
+fun countDigitsNumLess3(number: Int): Int =
+    when(number) {
+        in 0..9 -> 1
+        else -> countDigitsLess3(number)
+    }
+fun countDigitsLess3(number: Int): Int {
+    if (number == 0) return 0
+    return if (number % 10 < 3) 1 + countDigitsLess3(number/10)
+    else countDigitsLess3(number/10)
 }
