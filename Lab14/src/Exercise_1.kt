74ed5bd0719e23a18e32df672078ff327f4fad7d
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-05-09 21:56:04 +0300

    Add exercise 2

--- Lab14/src/Exercise_1.kt
+++ Lab14/src/Exercise_1.kt
@@ -1,8 +1,16 @@
 // 1
 fun sumDigitsNumUp(number: Int): Int =
     if (number == 0) 0
-    else (number % 10 + sumDigitsNumUp(number / 10))
+    else number % 10 + sumDigitsNumUp(number / 10)
+
+tailrec fun sumDigitsNumDown(number: Int, sum: Int): Int =
+    if (number == 0) sum else {
+        val num1 = number / 10
+        val sum1 = sum + (number % 10)
+        sumDigitsNumDown(num1,sum1)
+    }
 fun main() {
     val x = 915148
     println(sumDigitsNumUp(x))
+    println(sumDigitsNumDown(x,0))
 }
